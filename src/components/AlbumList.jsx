d4c0e60bfc305c777990498e6c45c6651f9a8d39
Mike Ta <h.michael.ta@gmail.com>
2020-01-29 20:21:52 +0100

    dummy data added, no api calls needed (mockaroo)

--- src/components/AlbumList.jsx
+++ src/components/AlbumList.jsx
@@ -1,18 +1,12 @@
 import React, { Component } from "react";
-import axios from "axios";
 import { Link } from "react-router-dom";
 import '../pages/Collection.css';
-import Navbar from "../components/Navbar.jsx"
-//import { getCollection } from "../utils/apiLogic"
+import collection from "../collection.json"
 
 //GET MY COLLECTION FROM DISCOGS
 export default class Collection extends Component {
     constructor(props) {
         super(props);
-
-        this.getCollection = this.getCollection.bind(this);
-        this.handleInputChange = this.handleInputChange.bind(this);
-        //this.getAlbumInfo = this.getAlbumInfo.bind(this);
 
         this.state = {
             collection: [],
@@ -21,37 +15,7 @@ export default class Collection extends Component {
     }
 
 
-    getCollection = () => {
-        axios
-            .get(
-                'https://api.discogs.com/users/michaeltaart/collection/folders/0/releases')
-            .then(response => {
-
-                this.setState({
-                    collection: response.data.releases,
-                    //album: response.data.releases.basic_information.title,
-                    error: null
-                });
-                
-            })
-            .catch(error => {
-                this.setState({
-                    error: error
-                });
-            });
-    };
-
-    handleInputChange(e) {
-        this.setState({
-            username: e.target.value
-        });
-        console.log("TEEEEEST", this.state.collection)
-        
-    }
-
-
     render() {
-        //let albumUri = encodeURI(this.state.album)
         return (
 
             <div className="collection-wrapper">
@@ -64,25 +28,22 @@ export default class Collection extends Component {
                             name="username"
                             type="text"
                             placeholder="Search by Discogs username"
-                            onChange={this.handleInputChange}
                         />
-                        <button onClick={this.getCollection}>Search</button>
+                        <button>Search</button>
                     </div>
 
                 </div>
-
 
 
                 <h1 className="title">My vinyl collection</h1>
 
                 <div className="release-output-wrapper">
                     <div className="release">
-                        {this.state.collection.map((release, index) => {
-
+                        {collection.map((release, index) => {
                             return (
                                 <div className="info" key={index}>
                                         <Link to={`/collection/${release.basic_information.title}`}>
-                                            <img className="covers" src="./images/LP_vinyl3.png" alt="lp" />
+                                            <img className="covers" src={release.basic_information.cover_image} alt="lp" />
                                         </Link>
 
                                     <div className="">
--- src/components/Tracklist.jsx
+++ src/components/Tracklist.jsx
@@ -1,93 +1,39 @@
 import React, { Component } from 'react';
-import axios from "axios";
 import '../pages/Collection.css';
-//import { Link } from "react-router-dom";
+import tracklist from "../tracklist.json"
 
 export default class Tracklist extends Component {
     constructor(props) {
         super(props)
-        this.getAlbumId = this.getAlbumId.bind(this);
-        this.getTrackList = this.getTrackList.bind(this);
-        this.getAlbumInfo = this.getAlbumInfo.bind(this);
 
         this.state = {
-            albumId: "",
-            trackList: [],
-            albumInfo: {},
-            albumTitle: "",
-            albumCover: "",
+            music: {
+                albumId: "",
+                trackList: [],
+                albumInfo: {},
+                albumTitle: "",
+                albumCover: "",
+            }
         }
     }
 
-    getAlbumId = () => {
-       
-        axios.get(
-            
-            'https://api.spotify.com/v1/search?q=thickfreakness&type=track'
 
-        )
-            .then(response => {
-                this.setState({
-                    albumId: response.data.tracks.items[0].album.id,
-                    albumInfo: response.data.tracks.items,
-                    albumTitle: response.data.tracks.items[0].name,
-                    artist: response.data.tracks.items[0].artists[0].name,
-                    albumCover: response.data.tracks.items[0].album.images[1].url,
-                    error: null
-                });
-            })
-            .catch((err) => { console.log("ERROR DURING AXIOS", err) })
-    };
-
-    getTrackList = () => {
-        axios.get(
-            'https://api.spotify.com/v1/albums/0GJH6shkenNdqkpGdsY8aa/tracks'
-        )
-            .then(response => {
-
-                this.setState({
-                    trackList: response.data.items,
-                    error: null
-                });
-            })
-            .catch((err) => { console.log("ERROR DURING AXIOS", err) })
-    }
-
-    getAlbumInfo() {
-        
-        this.getAlbumId();
-        this.getTrackList();
-        console.log("TEST",this.state.albumId)
-    }
-
-    componentDidMount() {
-        this.getAlbumInfo();
-    }
-
-    componentDidUpdate(prevProps) {
-        if (this.props.match.params.album_name !== prevProps.match.params.album_name) {
-            this.getAlbumInfo();
-        }
-    }
 
     render() {
-        
+
         return (
             <div className="tracklist">
                 <div className="release" >
-                    <button className="testbutton" onClick={this.getAlbumInfo} > GET INFO</button>
-
                     <div>
-                        <img src={this.state.albumCover} />
-                        <h1>{this.state.albumTitle}</h1>
-                        <h3>{this.state.artist}</h3>
+                        <img src="/images/dummy.jpeg"/>
+                        <h2>Thickfreakness</h2>
+                        <h3>The Black Keys</h3>
 
-                        {this.state.trackList.map((track, index) =>
+                        {tracklist.map((track, index) =>
                             <p key={index}>{index + 1}. {track.name}</p>
                         )}
                     </div>
                 </div>
-
             </div>
         )
     }
